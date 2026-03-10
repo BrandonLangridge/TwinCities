ca89b719b646639066cb05b2096c60be0debc445
coderrrr-sys <tylerdixon62@gmail.com>
2026-03-10 01:43:20 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,27 @@
 <?php
 /* config.php */
 
+// central configuration file for the TwinCities project
+
+//Error Reporting 
+// Enable display of all errors and warning during development 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Determine protocol and host dynamically
+// Determine protocol and host dynamically based on current request
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
+//get host (domain or IP)
 $host = $_SERVER['HTTP_HOST'];
+//making sure that theres base pasth(folder or script)
 $basePath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\'); 
 if ($basePath === '') $basePath = ''; // root folder fallback
+// combining the protocol, host and the base path to full basr URL
 $base_url = $protocol . '://' . $host . $basePath;
 
 // Define Settings Array
 $config = [
+    //DB settings
     "db" => [
         "host" => "localhost",
         "name" => "city_twin_db",
@@ -21,14 +29,14 @@ $config = [
         "pass" => "",
         "charset" => "utf8mb4" 
     ],
-
+    //API settings
     "api" => [
         "weather_base_url" => "https://api.open-meteo.com/v1/",
         "weather_units"    => "metric",
         "pixabay_key"      => "54664421-9a17e2d26b529b08d054890af"
     ],
-
-
+    
+    // queries
     "queries" => [
         "get_twin_cities" => "
             SELECT 
@@ -39,7 +47,7 @@ $config = [
             LIMIT 1
         " 
     ],
-
+    // RSS feed settings 
     "rss" => [
         "title"       => "Liverpool & Cologne - Places Feed",
         "description" => "Dynamic RSS generated from Cities and Place_of_Interest tables.",
@@ -50,10 +58,12 @@ $config = [
 
 // Establish PDO Connection
 try {
-    // Connect to the HOST only. 
-    // Connection works even if the DB doesn't exist yet.
+    // DSN for PDO connection
+    // connects to the host only 
     $dsn = "mysql:host=" . $config['db']['host'] . ";charset=" . $config['db']['charset'];
     $pdo = new PDO($dsn, $config['db']['user'], $config['db']['pass']);
+    
+    // sets PDO to throw exceptions on errors 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Only try to select the specific database if we are NOT on the setup page.
@@ -65,6 +75,7 @@ try {
 
     // If database does not exist, automatically run setup
     if (strpos($e->getMessage(), 'Unknown database') !== false) {
+        //redirects to setup.php to allow automatic creation
         header("Location: setup.php");
         exit;
     }
@@ -72,5 +83,6 @@ try {
     // Any other database error
     die("Database Connection Failed: " . $e->getMessage());
 }
-
+//Return config array so it may be included in other scripts
 return $config;
+
